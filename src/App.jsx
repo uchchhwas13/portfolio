657e23d520b932189b8a88745835a5fc062cecd9
Uchchhwas Roy <uchchhwas.roy.du@gmail.com>
2025-07-23 15:01:54 +0600

    Initial routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import { useState } from 'react'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { NotFound } from './pages/NotFound'
+import { Home } from './pages/Home'
 
 function App() {
 
   return (
     <>
-      
+      <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      </BrowserRouter>
     </>
   )
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+export const Home = () => {
+    return (
+        <div> It is Home 
+        {/*Theme Toggle*/}
+        {/*Backgrpound Effects*/}
+        {/*Navbar*/}
+        {/*Main Content*/}
+
+        
+        {/*Footer*/}
+        </div>
+    );
+}
