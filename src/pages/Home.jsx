187defa825bc72af0eefb9fce90af94002346f81
Uchchhwas Roy <uchchhwas.roy.du@gmail.com>
2025-07-24 14:17:35 +0600

    Completing about section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,8 @@ import { ThemeToggle } from "../components/ThemeToggle";
 import { StarBackground } from "../components/StarBackground";
 import { Navbar } from "../components/Navbar";
 import { HeroSection } from "../components/HeroSection";
+import { AboutSection } from "../components/AboutSection";
+
 export const Home = () => {
     return (
         <div className="min-h-screen bg-background text-foreground overflow-x-hidden"> 
@@ -14,6 +16,7 @@ export const Home = () => {
         {/*Main Content*/}
         <main>
             <HeroSection />
+            <AboutSection />
         </main>
 
         {/*Footer*/}
