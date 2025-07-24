d1be57966b8758b812bbd28e588d424a38504a4f
Uchchhwas Roy <uchchhwas.roy.du@gmail.com>
2025-07-24 12:10:22 +0600

    Adding nav bar for mobile and desktop deivces

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import { ThemeToggle } from "../components/ThemeToggle";
 import { StarBackground } from "../components/StarBackground";
+import { Navbar } from "../components/Navbar";
 export const Home = () => {
     return (
         <div className="min-h-screen bg-background text-foreground overflow-x-hidden"> 
@@ -8,7 +9,7 @@ export const Home = () => {
         {/*Backgrpound Effects*/}
         <StarBackground />
         {/*Navbar*/}
-
+        <Navbar />
         {/*Main Content*/}
 
 
