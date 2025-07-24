a467b7ff2d576d4f9e47d12cee3c2fe0bce42a23
Uchchhwas Roy <uchchhwas.roy.du@gmail.com>
2025-07-24 19:21:47 +0600

    Adding feedback section in contact

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -1,11 +1,17 @@
-import { Facebook, Github, Linkedin, Mail, MapPin, Phone, Twitter } from 'lucide-react';
+import { Facebook, Github, Linkedin, Mail, MapPin, Phone, Send, Twitter } from 'lucide-react';
+
 
 export const ContactSection = () => {
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    }
+
   return (
     <section id="contact" className="py-24 px-4 relative bg-secondary/30">
       <div className="container mx-auto max-w-5xl">
         <h2 className="text-3xl md:text-4xl font-bold mb-4 text-center">
-          Get In <span class="text-primary"></span>
+          Get In <span className="text-primary"></span>
         </h2>
         <p className="text-center mb-12 max-w-2xl mx-auto">
           Have a project in mind or want to collaborate? Feel free to reach out.
@@ -79,6 +85,35 @@ export const ContactSection = () => {
                 </a>
             </div>
             </div>
+          </div>
+          <div className="bg-card p-8 rounded-lg shadow-xs">
+            <h3 className="text-2xl font-semibold mb-6"> Send a Message</h3>
+            <form className="space-y-6">
+                <div>
+                    <label htmlFor='name' className='block text-sm font-medium mb-2'> Your Name</label>
+                    <input type='text' id='name' name='name' required
+                    className='w-full px-4 py-3 rounded-md border-input bg-background focus:outline-hidden focus:ring-primary'
+                    placeholder='Enter your name'/>
+                </div>
+
+                <div>
+                    <label htmlFor='email' className='block text-sm font-medium mb-2'> Your Email</label>
+                    <input type='email' id='email' name='email' required
+                    className='w-full px-4 py-3 rounded-md border-input bg-background focus:outline-hidden focus:ring-primary'
+                    placeholder='john@gmail.com'/>
+                </div>
+
+                <div>
+                    <label htmlFor='message' className='block text-sm font-medium mb-2'> Your Message</label>
+                    <textarea id='message' required
+                    className='w-full px-4 py-3 rounded-md border-input bg-background focus:outline-hidden focus:ring-primary resize-none'
+                    placeholder='Hello, I would like to hear from you'/>
+                </div>
+                <button type='submit' className='cosmic-button w-full flex items-center justify-center gap-2'>
+                    Send Message
+                    <Send size={16} />
+                </button>
+            </form>
           </div>
         </div>
       </div>
