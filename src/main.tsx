7c73c54d269944c2a1b41a839f8ae5f3b3738557
Uchchhwas Roy <uchchhwas.roy.du@gmail.com>
2025-07-30 11:38:21 +0600

    Confirming root element is not null

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+const root = document.getElementById('root');
+
+if (!root) {
+  throw new Error("Failed to find the root element.");
+}
+
+createRoot(root).render(
   <StrictMode>
     <App />
   </StrictMode>,
