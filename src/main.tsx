3beb8bf69f441ff09865f196819d2904aad34aa2
Uchchhwas Roy <uchchhwas.roy.du@gmail.com>
2025-08-07 14:28:37 +0600

    Updatin main file

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App';
 
 const root = document.getElementById('root');
 
-if (!root) {
-  throw new Error("Failed to find the root element.");
+if (root) {
+  createRoot(root).render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
 }
-
-createRoot(root).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
