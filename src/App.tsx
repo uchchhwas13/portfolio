feb65bef9eda2036e18ce2c7c967a24ab3d93525
Uchchhwas Roy <uchchhwas.roy.du@gmail.com>
2025-08-10 19:18:42 +0600

    Dropping unnecessary component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,9 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { NotFound } from './pages/NotFound';
 import { Home } from './pages/Home';
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
+      <Home />
     </>
   );
 }
