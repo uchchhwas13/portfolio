e5b1a04309367043cacafc0b2737dae32f6e7faa
Uchchhwas Roy <uchchhwas.roy.du@gmail.com>
2026-01-18 21:41:50 +0600

    Updating info

--- src/components/About/Intro.tsx
+++ src/components/About/Intro.tsx
@@ -3,7 +3,7 @@ export const Intro = () => {
     <div className="space-y-6">
       <h3 className="text-2xl font-semibold">Passionate Learner</h3>
       <p className="text-muted-foreground">
-        With over 4 years of experience in iOS app development, I’m now
+        With over 5 years of experience in iOS app development, I’m now
         transitioning into full-stack web development. I’m currently learning
         JavaScript, Node.js, React, MongoDB, and Tailwind CSS to build scalable,
         end-to-end applications.
